d6f69266b33d682c180edd0ba97501bd00e79527
Shidenkain1 <Stanislavdnepr1337@gmail.com>
2022-12-27 22:08:39 +0200

    Added web dependencies Added group and subject data sql script

---
+++ src/main/java/ua/com/foxminded/controller/HomePageController.java
@@ -0,0 +1,19 @@
+package ua.com.foxminded.controller;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HomePageController {
+
+    @Value("${spring.application.name}")
+    String applicationName;
+
+    @GetMapping("/")
+    public String homePage(Model model){
+        model.addAttribute("applicationName", applicationName);
+        return "home";
+    }
+}
---
+++ src/main/resources/university-data.sql
@@ -0,0 +1,79 @@
+INSERT INTO university.rooms(floor,number)
+VALUES(1,101);
+INSERT INTO university.rooms(floor,number)
+VALUES(1,102);
+INSERT INTO university.rooms(floor,number)
+VALUES(1,103);
+INSERT INTO university.rooms(floor,number)
+VALUES(1,104);
+INSERT INTO university.rooms(floor,number)
+VALUES(1,105);
+INSERT INTO university.rooms(floor,number)
+VALUES(1,106);
+INSERT INTO university.rooms(floor,number)
+VALUES(1,107);
+INSERT INTO university.rooms(floor,number)
+VALUES(1,108);
+INSERT INTO university.rooms(floor,number)
+VALUES(1,109);
+INSERT INTO university.rooms(floor,number)
+VALUES(1,110);
+INSERT INTO university.rooms(floor,number)
+VALUES(2,101);
+INSERT INTO university.rooms(floor,number)
+VALUES(2,102);
+INSERT INTO university.rooms(floor,number)
+VALUES(2,103);
+INSERT INTO university.rooms(floor,number)
+VALUES(2,104);
+INSERT INTO university.rooms(floor,number)
+VALUES(2,105);
+INSERT INTO university.rooms(floor,number)
+VALUES(2,106);
+INSERT INTO university.rooms(floor,number)
+VALUES(2,107);
+INSERT INTO university.rooms(floor,number)
+VALUES(2,108);
+INSERT INTO university.rooms(floor,number)
+VALUES(2,109);
+INSERT INTO university.rooms(floor,number)
+VALUES(2,110);
+INSERT INTO university.rooms(floor,number)
+VALUES(3,101);
+INSERT INTO university.rooms(floor,number)
+VALUES(3,102);
+INSERT INTO university.rooms(floor,number)
+VALUES(3,103);
+INSERT INTO university.rooms(floor,number)
+VALUES(3,104);
+INSERT INTO university.rooms(floor,number)
+VALUES(3,105);
+INSERT INTO university.rooms(floor,number)
+VALUES(3,106);
+INSERT INTO university.rooms(floor,number)
+VALUES(3,107);
+INSERT INTO university.rooms(floor,number)
+VALUES(3,108);
+INSERT INTO university.rooms(floor,number)
+VALUES(3,109);
+INSERT INTO university.rooms(floor,number)
+VALUES(3,110);
+
+INSERT INTO university.subjects(name)
+VALUES('Engineering Thermodynamics');
+INSERT INTO university.subjects(name)
+VALUES('Structure of Materials');
+INSERT INTO university.subjects(name)
+VALUES('Principles of Extractive Metallurgy');
+INSERT INTO university.subjects(name)
+VALUES('Phase Transformations');
+INSERT INTO university.subjects(name)
+VALUES('Introduction to Metallurgy and Materials Engineering');
+INSERT INTO university.subjects(name)
+VALUES('Metallurgical Thermodynamics and Kinetics');
+INSERT INTO university.subjects(name)
+VALUES('Mechanical Behaviour of Materials');
+INSERT INTO university.subjects(name)
+VALUES('Instrumental Analysis');
+INSERT INTO university.subjects(name)
+VALUES('Higher Mathematics');
