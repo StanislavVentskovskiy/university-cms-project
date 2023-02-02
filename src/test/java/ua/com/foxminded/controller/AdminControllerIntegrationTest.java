33ca61de263463161b93d091ee13f8995a0317aa
Shidenkain1 <Stanislavdnepr1337@gmail.com>
2023-02-02 18:00:54 +0200

    added spring security tests

---
+++ src/test/java/ua/com/foxminded/controller/AdminControllerIntegrationTest.java
@@ -0,0 +1,48 @@
+package ua.com.foxminded.controller;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
+import org.springframework.security.test.context.support.WithMockUser;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+import ua.com.foxminded.service.impl.RoleServiceImpl;
+import ua.com.foxminded.service.impl.UserServiceImpl;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@RunWith(SpringRunner.class)
+@WebMvcTest(AdminController.class)
+public class AdminControllerIntegrationTest {
+
+    @Autowired
+    private MockMvc mvc;
+    @MockBean
+    private UserServiceImpl userService;
+    @MockBean
+    private RoleServiceImpl roleService;
+
+    @WithMockUser(value = "admin", roles = {"ADMIN"})
+    @Test
+    public void givenAuthRequestOnPrivateServiceAdminRole_shouldReturn200() throws Exception {
+        mvc.perform(get("/login").contentType(MediaType.APPLICATION_JSON))
+            .andExpect(status().isOk());
+    }
+
+    @WithMockUser(value = "user", roles = {"USER"})
+    @Test
+    public void givenAuthRequestOnPrivateServiceUserRole_shouldReturn200() throws Exception {
+        mvc.perform(get("/login").contentType(MediaType.APPLICATION_JSON))
+            .andExpect(status().isOk());
+    }
+
+    @WithMockUser(value = "teacher", roles = {"TEACHER"})
+    @Test
+    public void givenAuthRequestOnPrivateServiceTeacherRole_shouldReturn200() throws Exception {
+        mvc.perform(get("/login").contentType(MediaType.APPLICATION_JSON))
+            .andExpect(status().isOk());
+    }
+}
